344a2ca3fbfd201ec345442d058225ad11e3fff8
amayk <amay.kalekar@gmail.com>
2019-01-12 12:42:24 +0530

    commit all changes

--- src/main/java/com/main/controller/ProfileController.java
+++ src/main/java/com/main/controller/ProfileController.java
@@ -1,15 +1,21 @@
 package com.main.controller;
 
-import javax.websocket.server.PathParam;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.main.service.ProfileService;
@@ -32,6 +38,11 @@ public class ProfileController {
 		return new ResponseEntity<ProfileVo>(profileService.getProfile(id),HttpStatus.OK);
 	}
 	
+	@GetMapping(value = "/api/profile/getProfileImage",produces = MediaType.IMAGE_JPEG_VALUE)
+	public @ResponseBody byte[] getImage() throws IOException {
+		FileInputStream imageOutFile = new FileInputStream("C:\\Users\\amayk\\images\\1.jpg");
+	    return IOUtils.toByteArray(imageOutFile);
+	}
 	
 	@PostMapping("/api/profile")
 	public ResponseEntity<ProfileVo> insertProfile(@RequestBody ProfileVo profileVo){
--- src/main/java/com/main/service/ProfileService.java
+++ src/main/java/com/main/service/ProfileService.java
@@ -1,5 +1,6 @@
 package com.main.service;
 
+import java.io.File;
 import java.io.FileOutputStream;
 
 import org.apache.tomcat.util.codec.binary.Base64;
@@ -10,40 +11,45 @@ import com.main.entity.ProfileEntity;
 import com.main.repository.ProfileRepository;
 import com.main.vo.ProfileVo;
 
-
 @Service
 public class ProfileService {
 
 	@Autowired
 	ProfileRepository repository;
-	
-	public ProfileVo getProfile(String id){
-		/*ProfileVo profileVo = new ProfileVo();
-		
-		profileVo.setFirstName("Suraj");
-		profileVo.setLastName("Patil");
-		profileVo.setPlaceOfBirth("pune");
-		profileVo.setDateOfBirth("28/08/1987");
-		profileVo.setAnnualIncome("8");*/
+
+	public ProfileVo getProfile(String id) {
+		/*
+		 * ProfileVo profileVo = new ProfileVo();
+		 * 
+		 * profileVo.setFirstName("Suraj"); profileVo.setLastName("Patil");
+		 * profileVo.setPlaceOfBirth("pune");
+		 * profileVo.setDateOfBirth("28/08/1987");
+		 * profileVo.setAnnualIncome("8");
+		 */
 		ProfileEntity entity = repository.findById(Long.valueOf(id)).get();
 		return new ProfileVo().getVo(entity);
 	}
-	
-	public void insertProfile(ProfileVo profileVo){
-		
-		try {
-			if(profileVo.getPicture1()!=null){
-			FileOutputStream imageOutFile = new FileOutputStream(
-					"C:\\Users\\amayk\\images\\test.jpg");
-			imageOutFile.write(Base64.decodeBase64(profileVo.getPicture1()));
 
-			imageOutFile.close();
+	public void insertProfile(ProfileVo profileVo) {
+
+		ProfileEntity entity = repository.save(profileVo.getEntity());
+		try {
+			if (profileVo.getPicture1() != null) {
+
+				File directory = new File(String.valueOf("C:\\Users\\amayk\\images\\" + entity.getId()));
+				if(!directory.exists())
+					directory.mkdirs();
+					
+				FileOutputStream imageOutFile = new FileOutputStream(
+						"C:\\Users\\amayk\\images\\" + entity.getId() + "\\1.jpg");
+				imageOutFile.write(Base64.decodeBase64(profileVo.getPicture1()));
+
+				imageOutFile.close();
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		repository.save(profileVo.getEntity());
-		
+
 	}
 }
